0661fc6cea9516f456919d5f08434ea909e58eff
fernvenue <git@fernvenue.com>
2022-02-10 05:51:40 +0000

    Update updater.sh, CIDR.conf, CIDR.txt, CIDR.yaml, LICENSE

--- updater.sh
+++ updater.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 eval $(ssh-agent -s)
-echo "$SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add -
+echo "$SSH_PRIVATE_KEY" | tr -d "\r" | ssh-add -
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
 ssh-keyscan gitlab.com > ~/.ssh/known_hosts
 chmod 644 ~/.ssh/known_hosts
 git config --global user.email "$GIT_MAIL_ADDRESS"
 git config --global user.name "fernvenue"
-git clone git@gilab.com:fernvenue/telegram-cidr-list.git
-cd './telegram-cidr-list'
-curl 'https://core.telegram.org/resources/cidr.txt' -o './CIDR.txt'
-sed -i 's/[[:space:]]//g' './CIDR.txt'
+git clone git@gitlab.com:fernvenue/telegram-cidr-list.git
+cd "./telegram-cidr-list"
+curl "https://core.telegram.org/resources/cidr.txt" > ./CIDR.txt
+sed -i "s/[[:space:]]//g" ./CIDR.txt
 cp ./CIDR.txt ./CIDR.yaml
-sed -i "s|^|  - '&|g" ./CIDR.yaml
+sed -i "s|^|- '&|g" ./CIDR.yaml
 sed -i "s|$|&'|g" ./CIDR.yaml
 sed -i "1s|^|payload:\n|" ./CIDR.yaml
 cp ./CIDR.txt ./CIDR.conf
