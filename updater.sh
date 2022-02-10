213e1cb44b3c0402b890043a89a3390f55d6ed71
fernvenue <git@fernvenue.com>
2022-02-10 05:31:26 +0000

    Update .gitlab-ci.yml, updater.sh

--- updater.sh
+++ updater.sh
@@ -3,11 +3,11 @@ eval $(ssh-agent -s)
 echo "$SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add -
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
-ssh-keyscan github.com > ~/.ssh/known_hosts
+ssh-keyscan gitlab.com > ~/.ssh/known_hosts
 chmod 644 ~/.ssh/known_hosts
-git config --global user.email "$GITHUB_MAIL_ADDRESS"
+git config --global user.email "$GIT_MAIL_ADDRESS"
 git config --global user.name "fernvenue"
-git clone git@github.com:fernvenue/telegram-cidr-list.git
+git clone git@gilab.com:fernvenue/telegram-cidr-list.git
 cd './telegram-cidr-list'
 curl 'https://core.telegram.org/resources/cidr.txt' -o './CIDR.txt'
 sed -i 's/[[:space:]]//g' './CIDR.txt'
@@ -19,5 +19,6 @@ cp ./CIDR.txt ./CIDR.conf
 sed -i "s|^|IP-CIDR,|g" ./CIDR.conf
 git init
 git add .
-git commit -m 'Update CIDR list'
+updated=`date --rfc-3339 sec`
+git commit -m "$updated"
 git push -u origin master
